505e1788760791f03ba7998dfeecc98178ce294a
Katharine Chui <katharine.chui@gmail.com>
2026-08-04 14:52:59 +0200

    adjust speaker and headphone volume

--- rootfs_build/vita.sh
+++ rootfs_build/vita.sh
@@ -3,8 +3,8 @@ echo s2idle > /sys/power/mem_sleep
 
 # audio path, oddly this isn't set correctly sometimes on UCM
 CARD=rockchipes8388c
-amixer -c $CARD cset name='Output 1 Playback Volume' 100%,100%
-amixer -c $CARD cset name='Output 2 Playback Volume' 100%,100%
+amixer -c $CARD cset name='Output 1 Playback Volume' 70%,70%
+amixer -c $CARD cset name='Output 2 Playback Volume' 70%,70%
 amixer -c $CARD cset name='PCM Volume' 100%,100%
 
 # let's not waste power on a lamp, it adds up when all we got is s2idle
